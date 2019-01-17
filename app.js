e8e08eb24103e0992e9b7d17a5ed528f30931391
coryswainston <swainston.cory89@gmail.com>
2019-01-17 00:57:33 -0700

    Makin some progress

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.set('port', (process.env.PORT || 5000))
   .set('views', __dirname + '/views')
@@ -8,7 +11,15 @@ app.set('port', (process.env.PORT || 5000))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/register', (req, res) => res.render('pages/register'))
   .get('/officers', (req, res) => res.render('pages/officers'))
+  .get('/poll', (req, res) => res.render('pages/poll'))
   .get('*', (req, res) => res.render('pages/404'))
   .listen(app.get('port'), () => {
     console.log("Node app is running at localhost:" + app.get('port'))
+  });
+
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
   });
