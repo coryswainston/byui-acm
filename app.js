16701ebd49fefbb9a57b31736aee1dd49aa17bf5
coryswainston <swainston.cory89@gmail.com>
2019-01-16 22:53:21 -0700

    Start to website

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+  .set('views', __dirname + '/views')
+  .set('view engine', 'ejs')
+  .use(express.static(__dirname + '/public'))
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/register', (req, res) => res.render('pages/register'))
+  .get('/officers', (req, res) => res.render('pages/officers'))
+  .get('*', (req, res) => res.render('pages/404'))
+  .listen(app.get('port'), () => {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  });
